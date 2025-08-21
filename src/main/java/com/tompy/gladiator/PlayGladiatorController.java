b6ac746b12a5d4280e4b49bd37adcf8223ea7f84
tompy <tompygo@comcast.net>
2025-08-21 10:29:12 -0500

    Hover moves works

--- src/main/java/com/tompy/gladiator/PlayGladiatorController.java
+++ src/main/java/com/tompy/gladiator/PlayGladiatorController.java
@@ -9,17 +9,33 @@ import com.tompy.hexboard.HexFunction;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.RadioButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PlayGladiatorController extends AbstractGameHexBoardController {
     private boolean moveSet = false;
     private Rectangle tempImage;
 
     @FXML
-    private CheckBox cbxQuick;
+    private CheckBox chbQuick;
+
+    @FXML
+    private RadioButton radPreRight;
+
+    @FXML
+    private RadioButton radPreLeft;
+
+    @FXML
+    private RadioButton radPostRight;
+
+    @FXML
+    private RadioButton radPostLeft;
 
     public PlayGladiatorController() {
 
@@ -52,99 +68,186 @@ public class PlayGladiatorController extends AbstractGameHexBoardController {
     private Hex findNext(Hex hex, int facing) {
         Hex[] neighbors = HexFunction.getNeighborArray(hex);
 
-        return neighbors[GladiatorData.get().getPlayer().getCounter().getFacing()];
+        return neighbors[facing];
+    }
+
+    private List<Hex> getMove(int directionOffset) {
+        return getMove(directionOffset, 1, false, false);
+    }
+
+    private List<Hex> getMove(int directionOffset, int length, boolean ignoreQuick, boolean ignoreRotationOffset) {
+        int offset = 0;
+        if (!ignoreRotationOffset && radPreRight.isSelected()) {
+            offset = -1;
+        }
+        if (!ignoreRotationOffset && radPreLeft.isSelected()) {
+            offset = 1;
+        }
+
+        List<Hex> returnValue = new ArrayList<>();
+        if (!moveSet) {
+            Counter counter = GladiatorData.get().getPlayer().getCounter();
+            int facing = counter.getFacing() - directionOffset - offset;
+            if (facing < 0) {
+                facing += 6;
+            }
+            if (facing > 5) {
+                facing -= 6;
+            }
+            Hex hex = findNext(counter.getHex(), facing);
+            returnValue.add(hex);
+            for (int i = 1; i < length; i++) {
+                hex = findNext(hex, facing);
+                returnValue.add(hex);
+            }
+
+            if (chbQuick.isSelected() && !ignoreQuick) {
+                returnValue.add(findNext(hex, facing));
+            }
+        }
+        return returnValue;
+    }
+
+    public void showHexes(List<Hex> hexes) {
+        for (Hex hex : hexes) {
+            hex.setFill(Color.PINK);
+            hex.setOpacity(0.6);
+        }
+    }
+
+    public void resetHexes(List<Hex> hexes) {
+        hexes.forEach(h -> h.setFill(Color.TRANSPARENT));
+    }
+
+    private void showHexImage(Hex hex, Counter counter) {
+        showHexImage(hex, counter, 0, false);
+    }
+
+    private void showHexImage(Hex hex, Counter counter, int rotation) {
+        showHexImage(hex, counter, rotation, false);
+    }
+
+    private void showHexImage(Hex hex, Counter counter, int rotation, boolean ignoreRotation) {
+        int facingOffset = 0;
+        if (!ignoreRotation && radPreRight.isSelected()) {
+            facingOffset = 1;
+        }
+        if (!ignoreRotation && radPreLeft.isSelected()) {
+            facingOffset = -1;
+        }
+        if (!ignoreRotation && radPostRight.isSelected()) {
+            facingOffset = 1;
+        }
+        if (!ignoreRotation && radPostLeft.isSelected()) {
+            facingOffset = -1;
+        }
+        tempImage = new Rectangle();
+        tempImage.setX(hex.localToParent(hex.getLayoutBounds()).getCenterX() - (counter.getImage().getWidth() / 2));
+        tempImage.setY(hex.localToParent(hex.getLayoutBounds()).getCenterY() - (counter.getImage().getHeight() / 2));
+        tempImage.setHeight(counter.getHeight());
+        tempImage.setWidth(counter.getWidth());
+
+        tempImage.setFill(new ImagePattern(counter.getImage()));
+        tempImage.setOpacity(0.5);
+
+        int fixedRotation = counter.getFacing() - 1 + rotation + facingOffset;
+        if (fixedRotation < 0) {
+            fixedRotation += 6;
+        }
+        if (fixedRotation > 5) {
+            fixedRotation -= 6;
+        }
+        tempImage.setRotate(fixedRotation * 60);
+
+        GameHexBoardData.get().getPaneHexBoard().getChildren().add(tempImage);
+    }
+
+    private void removeHexImage() {
+        GameHexBoardData.get().getPaneHexBoard().getChildren().remove(tempImage);
     }
 
     public void onForwardEnter(MouseEvent event) {
-        if (!moveSet) {
-            Counter counter = GladiatorData.get().getPlayer().getCounter();
-            Hex hex = findNext(counter.getHex(), counter.getFacing());
-
-            hex.setFill(Color.PINK);
-            hex.setOpacity(0.6);
-
-            if (cbxQuick.isSelected()) {
-                hex = findNext(hex, counter.getFacing());
-                hex.setFill(Color.PINK);
-                hex.setOpacity(0.6);
-            }
-
-            tempImage = new Rectangle();
-            tempImage.setX(hex.localToParent(hex.getLayoutBounds()).getCenterX() - (counter.getImage()
-                    .getWidth() / 2));
-            tempImage.setY(hex.localToParent(hex.getLayoutBounds()).getCenterY() - (counter.getImage()
-                    .getHeight() / 2));
-            tempImage.setHeight(counter.getHeight());
-            tempImage.setWidth(counter.getWidth());
-
-            tempImage.setFill(new ImagePattern(counter.getImage()));
-            tempImage.setOpacity(0.5);
-            tempImage.setRotate((counter.getFacing() - 1) * 60);
-            GameHexBoardData.get().getPaneHexBoard().getChildren().add(tempImage);
-        }
+        List<Hex> hexes = getMove(+0);
+        showHexes(hexes);
+        showHexImage(hexes.getLast(), GladiatorData.get().getPlayer().getCounter());
     }
 
     public void onForwardExit(MouseEvent event) {
-        if (!moveSet) {
-            Counter counter = GladiatorData.get().getPlayer().getCounter();
-            Hex hex = findNext(counter.getHex(), counter.getFacing());
-            hex.setFill(Color.TRANSPARENT);
-
-            if (cbxQuick.isSelected()) {
-                Hex quick = findNext(hex, counter.getFacing());
-                quick.setFill(Color.TRANSPARENT);
-            }
-
-            GameHexBoardData.get().getPaneHexBoard().getChildren().remove(tempImage);
-        }
+        List<Hex> hexes = getMove(0);
+        resetHexes(hexes);
+        removeHexImage();
     }
 
     public void onBackwardsEntered(MouseEvent event) {
-
+        List<Hex> hexes = getMove(3);
+        showHexes(hexes);
+        showHexImage(hexes.getLast(), GladiatorData.get().getPlayer().getCounter());
     }
 
     public void onBackwardsExited(MouseEvent event) {
-
+        List<Hex> hexes = getMove(3);
+        resetHexes(hexes);
+        removeHexImage();
     }
 
     public void onSSFLEntered(MouseEvent event) {
-
+        List<Hex> hexes = getMove(1);
+        showHexes(hexes);
+        showHexImage(hexes.getLast(), GladiatorData.get().getPlayer().getCounter());
     }
 
     public void onSSFLExited(MouseEvent event) {
-
+        List<Hex> hexes = getMove(1);
+        resetHexes(hexes);
+        removeHexImage();
     }
 
     public void onSSFREntered(MouseEvent event) {
-
+        List<Hex> hexes = getMove(-1);
+        showHexes(hexes);
+        showHexImage(hexes.getLast(), GladiatorData.get().getPlayer().getCounter());
     }
 
     public void onSSFRExited(MouseEvent event) {
-
+        List<Hex> hexes = getMove(-1);
+        resetHexes(hexes);
+        removeHexImage();
     }
 
     public void onSSBLEntered(MouseEvent event) {
-
+        List<Hex> hexes = getMove(2);
+        showHexes(hexes);
+        showHexImage(hexes.getLast(), GladiatorData.get().getPlayer().getCounter());
     }
 
     public void onSSBLExited(MouseEvent event) {
-
+        List<Hex> hexes = getMove(2);
+        resetHexes(hexes);
+        removeHexImage();
     }
 
     public void onSSBREntered(MouseEvent event) {
-
+        List<Hex> hexes = getMove(-2);
+        showHexes(hexes);
+        showHexImage(hexes.getLast(), GladiatorData.get().getPlayer().getCounter());
     }
 
     public void onSSBRExited(MouseEvent event) {
-
+        List<Hex> hexes = getMove(-2);
+        resetHexes(hexes);
+        removeHexImage();
     }
 
     public void onChargeEntered(MouseEvent event) {
-
+        List<Hex> hexes = getMove(0, 3, true, true);
+        showHexes(hexes);
+        showHexImage(hexes.getLast(), GladiatorData.get().getPlayer().getCounter(),0, true);
     }
 
     public void onChargeExited(MouseEvent event) {
-
+        List<Hex> hexes = getMove(0, 3, true, true);
+        resetHexes(hexes);
+        removeHexImage();
     }
 
     public void onPauseEntered(MouseEvent event) {
